ad3ccf53508b8573c4bf9b65c235ff8336765230
Aanchal Patial <aanchal@dunzo.in>
2023-06-22 13:41:42 +0530

    Initial Commit

---
+++ Hello Vision OS/Hello_Vision_OSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Vision_OSApp.swift
+//  Hello Vision OS
+//
+//  Created by Aanchal Patial on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Vision_OSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
